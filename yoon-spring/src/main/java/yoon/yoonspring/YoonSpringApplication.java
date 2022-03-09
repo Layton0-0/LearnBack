a71bb1cc11d17efcc07b1d2ddafe66e60cccd71c
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2022-03-09 22:16:51 +0900

    변경

---
+++ yoon-spring/src/main/java/yoon/yoonspring/YoonSpringApplication.java
@@ -0,0 +1,13 @@
+package yoon.yoonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonSpringApplication.class, args);
+	}
+
+}
